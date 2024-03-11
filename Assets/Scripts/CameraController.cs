c8c94757f01b9765a29c0d4655a1fe54286fa316
kailermai <kailermaioff@gmail.com>
2024-03-11 19:49:58 +0800

    added camera controls: zooming, rotating and moving the camera

--- Assets/Scripts/CameraController.cs
+++ Assets/Scripts/CameraController.cs
@@ -4,15 +4,60 @@ using UnityEngine;
 
 public class CameraController : MonoBehaviour
 {
-    // Start is called before the first frame update
+    public float moveSpeed;
+
+    public float minXRot;
+    public float maxXRot;
+    private float curXRot;
+    public float minZoom;
+    public float maxZoom;
+    public float zoomSpeed;
+    public float rotateSpeed;
+    private float curZoom;
+    private Camera cam;
+
     void Start()
     {
-        
+        cam = Camera.main;
+        curZoom = cam.transform.localPosition.y;
+        curXRot = -50;
     }
 
-    // Update is called once per frame
     void Update()
     {
-        
+        // zooming
+        curZoom += Input.GetAxis("Mouse ScrollWheel") * -zoomSpeed;
+        curZoom = Mathf.Clamp(curZoom, minZoom, maxZoom);
+
+        cam.transform.localPosition = Vector3.up * curZoom;
+
+        // camera look
+        if (Input.GetMouseButton(1))
+        {
+            float x = Input.GetAxis("Mouse X");
+            float y = Input.GetAxis("Mouse Y");
+
+            curXRot += -y * rotateSpeed;
+            curXRot = Mathf.Clamp(curXRot, minXRot, maxXRot);
+
+            transform.eulerAngles = new Vector3(curXRot, transform.eulerAngles.y + (x * rotateSpeed), 0.0f);
+        }
+
+        // movement
+        Vector3 forward = cam.transform.forward;
+        forward.y = 0.0f;
+        forward.Normalize();
+
+        Vector3 right = cam.transform.right;
+
+        float moveX = Input.GetAxisRaw("Horizontal");
+        float moveZ = Input.GetAxisRaw("Vertical");
+
+        Vector3 dir = forward * moveZ + right * moveX;
+        dir.Normalize();
+
+        dir *= moveSpeed * Time.deltaTime;
+
+        transform.position += dir;
     }
 }
